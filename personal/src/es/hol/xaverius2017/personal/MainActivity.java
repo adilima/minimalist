be8d929325faa2c7132cc1ecbdd803355390cbe5
FX. J. Adi Lima <adi7598@gmail.com>
2023-02-22 13:03:39 +0700

    added loadMarkdown function

--- personal/src/es/hol/xaverius2017/personal/MainActivity.java
+++ personal/src/es/hol/xaverius2017/personal/MainActivity.java
@@ -7,22 +7,25 @@ import android.widget.Toast;
 import android.webkit.WebView;
 import android.webkit.WebSettings;
 import android.content.Context;
+import java.io.InputStream;
 
 public class MainActivity extends Activity {
 
         @Override
         public void onCreate(Bundle savedInstanceBundle) {
                 super.onCreate(savedInstanceBundle);
-                // this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-                setContentView(R.layout.main);
-                WebView webView = (WebView) findViewById(R.id.webView1);
-		//setContentView(webView);
+                //this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+		// WebView webView = (WebView)R.layout.main;
+                //setContentView(webView);
+                WebView webView = new WebView(this);
+		setContentView(webView);
 
                 // enable javascript, and also the way to communicate with it 
                 webView.getSettings().setJavaScriptEnabled(true);
-                webView.addJavascriptInterface(new AndroidObject(this), "AndroidObject");
+                webView.addJavascriptInterface(new AndroidObject(this), "Personal");
                 // load HTML internal file 
                 webView.loadUrl("file:///android_asset/index.html");
+		// webView.loadUrl("https://contekan-2022.netlify.app");
         }
 
         class AndroidObject {
@@ -35,6 +38,24 @@ public class MainActivity extends Activity {
                 public void showToast(String strText) {
                         Toast.makeText(context, strText, Toast.LENGTH_SHORT).show();
                 }
+		public String loadMarkdown(String strPath) {
+		    String string = "Got an **empty** file.";
+		    InputStream inputStream = null;
+		    try {
+			inputStream = getAssets().open("markdown/" + strPath);
+			int size = inputStream.available();
+			if (size != 0) {
+			    byte[] buffer = new byte[size];
+			    inputStream.read(buffer);
+			    return new String(buffer);
+			}
+		    }
+		    catch (Exception e) {
+			return e.getMessage();
+		    }
+		    return string;
+		}
         }
 
 }
+
